b06f046f8cc2a5ff4ff6316dd6814595c6d20463
Mulongovictor <mulongovibu@gmail.com>
2023-08-11 16:56:31 -0700

    task 10

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,19 +9,20 @@
 */
 int main(void)
 {
-	char Lower = 'a';
-	char Upper = 'A';
+	int num_1, num_2;
+	num_1 = '0';
+	num_2 = '0';
 
-	while (Lower <= 'z')
+	while (num_1 <= '9')
 	{
-		putchar(Lower);
-		Lower++;
-	}
-	while (Upper <= 'Z')
-	{
-		putchar(Upper);
-		Upper++;
-	}
+		while (num_2 <= '9')
+		{
+			if (num_1 < num_2)
+			{
+				putchar(num_1);
+				putchar(num_2);
+				if (num_1 != '8' || (num_1 == '8' && num_2 != '9'))
+					put
 	putchar('\n');
 	return (0);
 }
