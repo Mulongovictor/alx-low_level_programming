dc728b15c66b63b5cfbd483c94c3f1bf5765b869
Mulongovictor <mulongovibu@gmail.com>
2023-08-11 13:40:42 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main -print alphabets in upper and
+ * lower case a-z
+ * Return: always (0)
+*/
+int main(void)
+{
+	char Lower = 'a';
+	char Upper = 'A';
+
+	while (Lower <= 'z')
+	{
+		putchar(Lower);
+		Lower++;
+	}
+	while (Upper <= 'Z')
+	{
+		putchar(Upper);
+		Upper++;
+	}
+	putchar('\n');
+	return (0);
+}
