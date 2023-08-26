7e37cddd2ae289a6d74d526b31285226fb8b4e4e
Mulongovictor <mulongovibu@gmail.com>
2023-08-26 15:51:35 -0700

    Task 0: A program that prints _putchar. followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main -Entry point
+ *Return: 0 Always
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\0');
+	return (0);
+} 
