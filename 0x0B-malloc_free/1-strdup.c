5972569c9566ed7641d9ce2036eb28c531902255
Mulongovictor <mulongovibu@gmail.com>
2023-09-30 06:29:46 -0700

    Testing my solution

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,32 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
-*/
+ * _strdup - a string is parsed and duplicated into allocated
+ * memory.
+ * @str: is the string to be copied in allocated memory
+ *
+ * Return: ptr, the string in allocated memory
+ */
 
 char *_strdup(char *str)
 {
-	char *aaa;
-	int i, r = 0;
+	char *ptr;
+	int i = 0, j;
+	int count = 0;
 
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	aaa = malloc(sizeof(char) * (i + 1));
-	if (aaa == NULL)
-		return (NULL);
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
-	return (aaa);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	ptr = malloc(sizeof(char) * count);
+
+	for (j = 0; j < count; j++)
+	{
+		ptr[j] = str[j];
+	}
+	return (ptr);
+
 }
