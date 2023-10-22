11a121d5f13c1857f2d9756e3947912c111db064
Mulongovictor <mulongovibu@gmail.com>
2023-10-22 09:03:51 -0700

    Your commit message here

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,8 @@
 
 /**
  * main - This program will assign a random number to the
- * variable n each time it is executed. Complete the source
- * it print a message whether the number stored in the
+ * variable n each time it is executed.
+ * It prints a message whether the number stored in the
  * variable n is positive or negative.
  *
  *Return: Always 0 (Success)
