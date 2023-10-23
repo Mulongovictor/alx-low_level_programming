16a7c51579cef40a945dac6d83c33214efed2845
Mulongovictor <mulongovibu@gmail.com>
2023-10-22 23:49:17 -0700

    Programs prints the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,9 +5,9 @@
 
 /**
  * main - This program will assign a random number to
- * the variable n each time it is executed. Complete the source
- * code in order to print the last digit of the number
- * stored in the variable n.
+ * the variable n each time it is executed. Complete
+ * it  prints the last digit of the number stored in the variable n.
+ *
  * Return: Always (0)
 */
 int main(void)
