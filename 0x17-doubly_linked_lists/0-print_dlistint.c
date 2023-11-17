50e0a2c818d73b6a819b1d2d769077d4407230b7
Mulongovictor <mulongovibu@gmail.com>
2023-11-17 08:45:21 -0800

    A function that prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,7 +12,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 	{
-		printf("The stack is empty");
+		printf("The stack is empty\n");
 	}
 	else
 	{
@@ -20,7 +20,7 @@ size_t print_dlistint(const dlistint_t *h)
 		{
 			printf("%d", h->n);
 			h = h->next;
-			count++;
+			count += 1;
 			printf("\n");
 		}
 	}
